ab2491c3dcd1a62e14be4330933872c2d54ebf3e
Max Aryus <60442357+MaxAryus@users.noreply.github.com>
2020-04-06 13:28:03 +0200

    Update TaskTimer.swift

--- Task Timer/Views/TaskTimer.swift
+++ Task Timer/Views/TaskTimer.swift
@@ -8,7 +8,6 @@
 
 import SwiftUI
 
-
 struct TaskTimer: View {
     
 //    MARK: - Properties
@@ -16,121 +15,101 @@ struct TaskTimer: View {
     @State var sliderValue: Double = 5.0
     @State var showSlider: Bool = true
     
+    @State private var currentDate: Date?
+    @State private var beginningDate: Date?
+    @State private var endDate: Date?
+    @State private var minutesleft: Int?
+    @State private var timerIsRunning: Bool = false
     
-    @State var currentDate: Date?
-    @State var timerIsRunning: Bool = false
-    @State var label: Int = 5
-    @State var beginningDate: Date?
-    @State var endDate: Date?
-    @State var minutesLeft: Int?
-//    MARK: - Content
     
     var body: some View {
         VStack {
             HStack {
-                VStack {
-//  Contains the Timerlabel view
-                    ZStack {
-                        Circle()
-                            .frame(width: 185, height: 185, alignment: .center).shadow(color: .gray, radius: 9, x: 0, y: 3)
-                            .overlay(
-                                Circle()
-                                    .stroke(green, lineWidth: 4)
-                        ).foregroundColor(.white)
-                        Text("\(self.timerIsRunning ? self.label : Int(self.sliderValue))")
-                            .font(.custom("Nunito-Bold", size: 40)).foregroundColor(gray)
-                    }
-                    
+                ZStack {
+                    Circle()
+                        .frame(width: 185, height: 185, alignment: .center).shadow(color: gray, radius: 9, x: 0, y: 3)
+                        .overlay(
+                            Circle().stroke(green, lineWidth: 6)
+                    ).foregroundColor(.white)
+                    Text("\(Int(self.sliderValue))")
+                        .font(.custom("Nunito-Bold", size: 40)).foregroundColor(gray)
                 }
             }.padding()
-//  Touchgesture for hiding the slider
+//  Touchgesture Double Tap -> Show/ hide Slider, if timer is Running Delete Timer
                 .gesture(TapGesture(count: 2)
                     .onEnded({ (_) in
-                        // ShowSlider, deleteTimer
-                        if self.timerIsRunning == false {
+                        if self.timerIsRunning {
+                            print("Countdown Delete")
+                        } else if self.timerIsRunning == false {
                             self.showSlider.toggle()
-                        } else if self.timerIsRunning == true {
-                            self.deleteCountDown()
                         }
-
                     })
             )
-                
-//  Touchgesture for starting and stopping the Countdown
-                    .gesture(TapGesture(count: 1)
-                        .onEnded({ (_) in
-                           // Start, Pause timer
-                            self.timerIsRunning.toggle()
-                            self.startTimer()
-                        })
-                )
-                
-//  Slider value stepping with 5
+//  Touchgesture one Tap -> Start/ Stop countdown
+                .gesture(TapGesture(count: 1)
+                    .onEnded({ (_) in
+                        self.timerIsRunning.toggle()
+                        self.statTimer()
+                    })
+            )
+            
+//  SliderView
             HStack {
                 if showSlider {
-                    Slider(value: $sliderValue, in: 5...60, step: 5).padding([.leading, .trailing], 20).background(shadow(radius: 6))
+                    Slider(value: $sliderValue, in: 5.0...60.0, step: 5.0)
                 }
-                
-            }
+            }.padding([.trailing, .leading], 20)
+            
         }
     }
-//    MARK: - Functions
     
-    func deleteCountDown() {
-        timer.invalidate()
-        self.minutesLeft = 0
-        self.timerIsRunning = false
-        self.showSlider     = true
-        
-    }
+//      MARK: - Functions
     
-    func countDown() {
-        if self.minutesLeft! < 0 {
-            self.label = self.minutesLeft! * (-1)
-        } else {
-            timer.invalidate()
-            self.timerIsRunning = false
-            self.showSlider = true
-        }
-        
-    }
-    
-    
-    func startTimer() {
-        
+    func statTimer() {
         if timerIsRunning {
             self.showSlider = false
             
             let date = NSDate()
             let components = calendar.dateComponents([.hour, .minute,], from: date as Date)
+            
             self.beginningDate = calendar.date(from: components)
+            self.endDate       = calendar.date(byAdding: .minute, value: Int(sliderValue), to: self.beginningDate!)
             
-            self.endDate = calendar.date(byAdding: .minute, value: Int(sliderValue), to: self.beginningDate!)
-            
-        timer = Timer.scheduledTimer(withTimeInterval: TimeInterval(seconds * 30), repeats: true, block: { (_) in
+            timer = Timer.scheduledTimer(withTimeInterval: TimeInterval(seconds * 30), repeats: true, block: { (_) in
                 
                 let date = NSDate()
                 let components = calendar.dateComponents([.hour, .minute], from: date as Date)
                 self.currentDate = calendar.date(from: components)
                 
-                let difference = calendar.dateComponents([.hour, .minute], from: self.endDate! as Date, to: self.currentDate!)
+                let differnce = calendar.dateComponents([.hour, .minute], from: self.endDate! as Date, to: self.currentDate!)
                 
-
+                var hoursLeft = differnce.hour
+                self.minutesleft = differnce.minute
                 
-                var hoursLeft = difference.hour
-                self.minutesLeft = difference.minute
-                                
                 if hoursLeft! > 0 {
                     hoursLeft! -= 1
-                    self.minutesLeft! += 60
+                    self.minutesleft! += 60
                 }
                 
+                self.minutesleft = self.minutesleft! * (-1)
+                
                 self.countDown()
+                
             })
-            
+        
         }
-          
     }
+    
+    func countDown() {
+        if self.minutesleft! > 0 {
+            self.sliderValue = Double(self.minutesleft!)
+        } else {
+            timer.invalidate()
+            self.timerIsRunning = false
+            self.showSlider = true
+        }
+    }
+    
     
 }
 
@@ -139,9 +118,8 @@ struct TaskTimer: View {
 
 //      MARK: - Preview
 
-struct TaskTimer_Previews: PreviewProvider {
+struct TaskTimer_Preview: PreviewProvider {
     static var previews: some View {
         TaskTimer()
     }
 }
-
