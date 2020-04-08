dd1099b1d486b17c0816c9cc2bbb6f47c74b3c3a
Max Aryus <60442357+MaxAryus@users.noreply.github.com>
2020-04-08 02:53:01 +0200

    Update ContentView.swift

--- Task Timer/Views/ContentView.swift
+++ Task Timer/Views/ContentView.swift
@@ -11,35 +11,72 @@ import SwiftUI
 
 struct ContentView: View {
 
-    
-    
-    
-    @State var showSlider: Bool = true
-    
-    
-    
-    
     @State var workMinutes: Int = 0
+    @State var showMenu: Bool = false
+    
+    var body: some View {
+        
+        let drag = DragGesture()
+            .onEnded {
+                if $0.translation.width < -100 {
+                    withAnimation {
+                        self.showMenu = false
+                    }
+                }
+        }
+        
+        let dragToMenue = DragGesture()
+            .onEnded {
+                if $0.translation.width < 300 {
+                    withAnimation {
+                        self.showMenu = true
+                    }
+                }
+        }
+
+        return GeometryReader { geometry in
+                ZStack(alignment: .leading) {
+                    MainView(workMinutes: self.$workMinutes, showMenu: self.$showMenu)
+                        .frame(width: geometry.size.width, height: geometry.size.height)
+                        .offset(x: self.showMenu ? geometry.size.width/2 : 0)
+                        .gesture(dragToMenue)
+                    if self.showMenu {
+                        MenuView()
+                            .frame(width: geometry.size.width/2)
+                            .transition(.move(edge: .leading))
+                    }
+                    
+                }
+                .gesture(drag)
+            }
+        }
+    
+}
+    
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+struct MainView: View {
+    
+    @Binding var workMinutes: Int
+    @Binding var showMenu: Bool
     
     var body: some View {
         VStack {
             HStack(alignment: .bottom) {
-                VStack(spacing: 3) {
-                    Image(systemName: "square.fill")
-                        .resizable()
-                        .frame(width: 11, height: 11)
-                        .foregroundColor(blue)
-                    Image(systemName: "square.fill")
-                        .resizable()
-                        .frame(width: 11, height: 11)
-                        .foregroundColor(blue)
-                    Image(systemName: "square.fill")
-                        .resizable()
-                        .frame(width: 11, height: 11)
-                        .foregroundColor(blue)
+                Button(action: {
+                    withAnimation {
+                        self.showMenu.toggle()
+                    }
                     
-                }.shadow(color: .gray, radius: 6, x: 0, y: 3)
-                .padding(.leading, 21)
+                }) {
+                    MenueButton()
+                }
                 Spacer()
                 VStack {
                     Text("\(workMinutes) Minutes \n worked today")
@@ -51,7 +88,7 @@ struct ContentView: View {
                 }
             }.padding(.top)
             
-            TaskTimer()
+            TaskTimer(workedTime: $workMinutes)
             
             Spacer()
             HStack {
@@ -63,7 +100,7 @@ struct ContentView: View {
                 Image("addButton")
                 .resizable()
                     .frame(width: 55, height: 55).padding(.trailing, 21)
-                    .shadow(color: .gray, radius: 9, x: 0, y: 3)
+                    .shadow(color: gray, radius: 6, x: 0, y: 3)
                 
             }
             Spacer()
@@ -86,15 +123,29 @@ struct ContentView: View {
                 }
                 
             }.shadow(color: .gray, radius: 9, x: 0, y: 3)
-                .edgesIgnoringSafeArea(.bottom)
+                
             
-        }
+        }.edgesIgnoringSafeArea(.bottom)
     }
-    
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+struct MenueButton: View {
+    var body: some View {
+        VStack(spacing: 3) {
+            Image(systemName: "square.fill")
+                .resizable()
+                .frame(width: 11, height: 11)
+                .foregroundColor(blue)
+            Image(systemName: "square.fill")
+                .resizable()
+                .frame(width: 11, height: 11)
+                .foregroundColor(blue)
+            Image(systemName: "square.fill")
+                .resizable()
+                .frame(width: 11, height: 11)
+                .foregroundColor(blue)
+            
+        }.shadow(color: .gray, radius: 6, x: 0, y: 3)
+            .padding(.leading, 21)
     }
 }
