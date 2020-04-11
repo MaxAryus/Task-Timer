81dcd2f5133ea81b94a08f56986c19c544d28a00
Max Aryus <60442357+MaxAryus@users.noreply.github.com>
2020-04-12 01:08:16 +0200

    Update ContentView.swift

--- Task Timer/Views/ContentView.swift
+++ Task Timer/Views/ContentView.swift
@@ -8,7 +8,6 @@
 
 import SwiftUI
 
-
 struct ContentView: View {
 
     @State var workMinutes: Int = 0
@@ -16,40 +15,21 @@ struct ContentView: View {
     
     var body: some View {
         
-        let drag = DragGesture()
-            .onEnded {
-                if $0.translation.width < -100 {
-                    withAnimation {
-                        self.showMenu = false
-                    }
+    return GeometryReader { geometry in
+            ZStack(alignment: .leading) {
+                MainView(showMenu: self.$showMenu)
+                    .frame(width: geometry.size.width, height: geometry.size.height)
+                    .offset(x: self.showMenu ? geometry.size.width/2 : 0)
+                if self.showMenu {
+                    MenuView()
+                        .frame(width: geometry.size.width/2)
+                        .transition(.move(edge: .leading))
                 }
-        }
-        
-        let dragToMenue = DragGesture()
-            .onEnded {
-                if $0.translation.width < 300 {
-                    withAnimation {
-                        self.showMenu = true
-                    }
-                }
-        }
-
-        return GeometryReader { geometry in
-                ZStack(alignment: .leading) {
-                    MainView(workMinutes: self.$workMinutes, showMenu: self.$showMenu)
-                        .frame(width: geometry.size.width, height: geometry.size.height)
-                        .offset(x: self.showMenu ? geometry.size.width/2 : 0)
-                        .gesture(dragToMenue)
-                    if self.showMenu {
-                        MenuView()
-                            .frame(width: geometry.size.width/2)
-                            .transition(.move(edge: .leading))
-                    }
-                    
-                }
-                .gesture(drag)
+                
             }
         }
+    
+    }
     
 }
     
@@ -62,90 +42,16 @@ struct ContentView_Previews: PreviewProvider {
 }
 
 struct MainView: View {
-    
-    @Binding var workMinutes: Int
+
     @Binding var showMenu: Bool
     
     var body: some View {
         VStack {
-            HStack(alignment: .bottom) {
-                Button(action: {
-                    withAnimation {
-                        self.showMenu.toggle()
-                    }
-                    
-                }) {
-                    MenueButton()
-                }
-                Spacer()
-                VStack {
-                    Text("\(workMinutes) Minutes \n worked today")
-                        .multilineTextAlignment(.center)
-                        .lineLimit(3)
-                        .padding(.trailing)
-                        .font(.custom("Nunito-SemiBold", size: 11))
-                        .shadow(color: .gray, radius: 6, x: 0, y: 3)
-                }
-            }.padding(.top)
-            
-            TaskTimer(workedTime: $workMinutes)
-            
+            TaskTimer(showMenu: $showMenu)
             Spacer()
-            HStack {
-                Text("1 Task remaining")
-                    .font(.custom("Nunito-Bold", size: 11))
-                    .shadow(color: .gray, radius: 6, x: 0, y: 3)
-                    .padding(.leading, 21)
-                Spacer()
-                Image("addButton")
-                .resizable()
-                    .frame(width: 55, height: 55).padding(.trailing, 21)
-                    .shadow(color: gray, radius: 6, x: 0, y: 3)
-                
-            }
             Spacer()
-            
-            List {
-                HStack {
-                    VStack(alignment: .leading) {
-                        Text("Homework")
-                            .font(.custom("Nunito-SemiBold", size: 20))
-                            .foregroundColor(gray)
-                        Text("10 Minutes past")
-                            .font(.custom("Nunito-ExtraLight", size: 11))
-                            .foregroundColor(gray)
-                    }
-                    Spacer()
-                    Image(systemName: "square")
-                        .resizable()
-                        .foregroundColor(green)
-                        .frame(width: 22, height: 22)
-                }
-                
-            }.shadow(color: .gray, radius: 9, x: 0, y: 3)
-                
-            
+            TaskListViewModel()
         }.edgesIgnoringSafeArea(.bottom)
     }
 }
 
-struct MenueButton: View {
-    var body: some View {
-        VStack(spacing: 3) {
-            Image(systemName: "square.fill")
-                .resizable()
-                .frame(width: 11, height: 11)
-                .foregroundColor(blue)
-            Image(systemName: "square.fill")
-                .resizable()
-                .frame(width: 11, height: 11)
-                .foregroundColor(blue)
-            Image(systemName: "square.fill")
-                .resizable()
-                .frame(width: 11, height: 11)
-                .foregroundColor(blue)
-            
-        }.shadow(color: .gray, radius: 6, x: 0, y: 3)
-            .padding(.leading, 21)
-    }
-}
